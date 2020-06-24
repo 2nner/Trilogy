5c9e83597425e5abe612a0c2d56e4f07017b1775
2nner <khsbs001@soongsil.ac.kr>
2020-06-24 16:13:21 +0900

    프로젝트 초기 세팅 및 필요 라이브러리 implementation

---
+++ app/src/main/java/com/khsbs/trilogy/ui/main/InterpretActivity.kt
@@ -0,0 +1,12 @@
+package com.khsbs.trilogy.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.khsbs.trilogy.R
+
+class InterpretActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interpret)
+    }
+}
---
+++ app/src/main/java/com/khsbs/trilogy/ui/main/InterpretViewModel.kt
@@ -0,0 +1,7 @@
+package com.khsbs.trilogy.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class InterpretViewModel : ViewModel() {
+
+}
