62c3b6478dd4c229abc0be518dc853c50c1563d4
2nner <khsbs001@soongsil.ac.kr>
2020-07-25 14:20:25 +0900

    이전 커밋에서 올라가지 않은 파일 추가

---
+++ app/src/main/java/com/khsbs/trilogy/repository/entity/InterpretHistory.kt
@@ -0,0 +1,14 @@
+package com.khsbs.trilogy.repository.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "history")
+data class InterpretHistory (
+    @PrimaryKey val inputText: String,
+    val sourceLang: String,
+    val targetLang: String,
+    val resultPapago: String?,
+    val resultKakaoi: String?,
+    val resultGoogle: String?
+)
